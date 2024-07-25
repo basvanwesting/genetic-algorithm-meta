083625159031c4a03f85c7a7ed7a495a8969d862
basvanwesting <basvanwesting@hotmail.com>
2024-07-25 07:25:16 +0200

    initial commit

---
+++ src/prelude.rs
@@ -0,0 +1,9 @@
+#[doc(no_inline)]
+pub use crate::config::{
+    Config as MetaConfig, ConfigBuilder as MetaConfigBuilder,
+    TryFromConfigBuilderError as MetaTryFromConfigBuilderError,
+};
+#[doc(no_inline)]
+pub use crate::permutate::Permutate as MetaPermutate;
+#[doc(no_inline)]
+pub use crate::stats::Stats as MetaStats;
---
+++ tests/support/build.rs
@@ -0,0 +1,40 @@
+use genetic_algorithm::chromosome::Chromosome;
+use genetic_algorithm::fitness::FitnessValue;
+use genetic_algorithm::genotype::Genotype;
+use genetic_algorithm::population::Population;
+
+#[allow(dead_code)]
+pub fn chromosome<T: Genotype>(genes: Vec<T::Allele>) -> Chromosome<T> {
+    Chromosome::new(genes)
+}
+
+#[allow(dead_code)]
+pub fn chromosome_with_fitness_score<T: Genotype>(
+    genes: Vec<T::Allele>,
+    fitness_score: Option<FitnessValue>,
+) -> Chromosome<T> {
+    Chromosome {
+        genes,
+        fitness_score,
+        age: 0,
+    }
+}
+
+#[allow(dead_code)]
+pub fn population<T: Genotype>(data: Vec<Vec<T::Allele>>) -> Population<T> {
+    let chromosomes = data.into_iter().map(|genes| chromosome(genes)).collect();
+
+    Population::new(chromosomes)
+}
+
+#[allow(dead_code)]
+pub fn population_with_fitness_scores<T: Genotype>(
+    data: Vec<(Vec<T::Allele>, Option<FitnessValue>)>,
+) -> Population<T> {
+    let chromosomes = data
+        .into_iter()
+        .map(|tuple| chromosome_with_fitness_score(tuple.0, tuple.1))
+        .collect();
+
+    Population::new(chromosomes)
+}
